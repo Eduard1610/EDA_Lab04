145e98092c03e43dcdba09cc31393a19fdbfd8bb
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-06-12 13:08:48 -0500

    Creando clase Test y probando la lista enlazada

---
+++ Ejercicio01/Test.java
@@ -0,0 +1,26 @@
+public class Test {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> lint = new LinkedList<Integer>();
+
+		// LLENANDO LA LISTA
+		lint.insertFirst(10);
+		System.out.println(lint);
+		lint.insertFirst(14);
+		System.out.println(lint);
+		lint.insertFirst(7);
+		System.out.println(lint);
+		lint.insertFirst(23);
+		System.out.println(lint);
+		lint.insertFirst(18);
+		System.out.println(lint);
+		lint.insertFirst(6);
+		System.out.println(lint);
+		lint.insertFirst(19);
+		System.out.println(lint);
+		lint.insertFirst(25);
+		System.out.println(lint);
+		
+
+	}
+}
