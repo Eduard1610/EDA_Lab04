696a16acb1f7fd2cf2d8e7396173184e26aeca3e
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-06-12 14:24:06 -0500

    Implementando funcion InsertionSort

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -44,10 +44,46 @@ public class DoubleLinkedList <E extends Comparable<E>>{
        }
    }
 
+   public long insertionSort(){//Ordena la lista
+        Node <E> key;//variable auxiliar - almacenara un nodo
+	      int i;
+        long nano_startTime = System.nanoTime();//Inicio de contador de tiempo de ejecucion
+	      for (int j=1; j< tamano; j=j+1) {
+            key = this.get(j);//Se almacena el nodo en j (empezando desde la posicion 1)
+            i=j-1;
+            while(i>-1 && this.get(i).getData().compareTo(key.getData())>0)//Evalua si los nodos en i son menores al nodo en j
+                i--;//Si es cierto se reduce el indice i
+            if(i != j-1){//En caso de que hubo alguna reduccion en el indice i
+                this.remove(j);//Se remueve el nodo en j (este aun se encuentra almacenado en key)
+                tamano++;//debido al remove, la lista se tomara como una lista de n-1 elementos, sin embargo, no se esta eliminando permanentemente sino que solo se hara un reordenamiento de nodos
+                if(i == -1){//Caso: La comparacion llego hasta el primer elementos
+                    key.setNext(this.root);//el nextNode de key sera el nodo root
+                    this.root.setPrevious(key);//el previousNode de root sera key
+                    this.root = key;//El nuevo nodo root sera key
+                }
+                else{//Caso: La comparacion llego a un nodo distinto del nodo root
+                    this.get(i+1).setPrevious(key);//Se hace las vinculacion respectivas de nextNode y previousNode
+                    key.setPrevious(this.get(i));//Ademas se considera que el indice i señala a una posicion anterior
+                    key.setNext(this.get(i+1));// en la que deberia insertarse el nodo con indice j
+                    this.get(i).setNext(key);
+                }
+            }
+	      }
+        long nano_endTime = System.nanoTime();//Fin de contador de tiempo de ejecucion
+	      return nano_endTime - nano_startTime;//devuelve el tiempo total de ejecucion
+    }
+
 
     //Metodos auxiliares--------------------------------------
 
     public boolean isEmpty(){//Verifica si la lista esta vacia
         return this.root == null;
+    }
+    public String toString(){//Imprime los elementos de la lista
+        String str = "";
+        for(int i = 0; i < tamano; i++){
+            str += this.get(i).getData() + " ";
+        }
+        return str;
     }
 }
