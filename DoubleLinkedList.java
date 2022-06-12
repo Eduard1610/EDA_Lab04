d156234107335ed926def0278b330d12af48f824
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-06-12 12:42:45 -0500

    Implementando funcion de add(agregar nodo) y isEmpty

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -7,4 +7,21 @@ public class DoubleLinkedList <E extends Comparable<E>>{
         last = null;
         tamano = 0;
     }
+    public void add(E x){
+        Node <E> aux = new Node(x);
+        if(isEmpty())//Si esta vacia la lista
+            this.root = aux;//El nodo cabeza sera el nodo agregado
+        else{//Si tiene por lo menos un elemento
+            this.last.setNext(aux);//El nextNode de last sera el nuevo nodo creado
+            aux.setPrevious(this.last);//Se establece que previousNode sera el last
+        }
+        this.last = aux;//Se establece que el nodo creado sera ahora el ultimo de la lista
+        tamano++;//Se incrementa la cantidad del tamaño de la lista
+    }
+
+    //Metodos auxiliares--------------------------------------
+
+    public boolean isEmpty(){//Verifica si la lista esta vacia
+        return this.root == null;
+    }
 }
