48e8f4a897d483cb006cec236bbc12491b066b11
M-UNSA <82843764+M-UNSA@users.noreply.github.com>
2022-06-12 19:46:57 -0500

    Primer intento

--- Ejercicio02/test.java
+++ Ejercicio02/test.java
@@ -1,15 +1,33 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 import com.panayotis.gnuplot.JavaPlot; 
 
 public class test {
 
+    public static int[] generarPeorCaso(int t) {
+        int[] lista= new int[t];
+        for (int i=0; i<t; i++) {
+            lista[i] = t-i;		
+        }
+        return lista;		
+    }
+
     public static void main(String[] args) {
 
-        DoubleLinkedList<E> list = new DoubleLinkedList<>();
-        JavaPlot p = new JavaPlot();
-
-        p.addPlot("sin(x)");
-
-        p.plot();
+        int tamano;		        
+        Scanner teclado = new Scanner( System.in );
+        System.out.print( "Introduzca el tamaño máximo del último arreglo: " );
+        tamano = teclado.nextInt();
+        
+        ArrayList<int[]> casos = new ArrayList<int[]>();        
+        for(int n=1; n<=tamano; n++) {
+            casos.add(generarPeorCaso(n));
+        }        
+        DoubleLinkedList<int[]> puntero = casos.DoubleLinkedList();
+        while(puntero.hasNext()){
+            System.out.println(puntero.next());
+        }
 
     }
 
