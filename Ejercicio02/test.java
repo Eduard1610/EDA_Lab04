7c3b55f7f1354ed072bc192982d5019de5f5e6af
M-UNSA <82843764+M-UNSA@users.noreply.github.com>
2022-06-12 23:08:41 -0500

    Aplicando DoubleLinkedList correctamente

--- Ejercicio02/test.java
+++ Ejercicio02/test.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -5,30 +7,35 @@ import com.panayotis.gnuplot.JavaPlot;
 
 public class test {
 
-    public static int[] generarPeorCaso(int t) {
-        int[] lista= new int[t];
-        for (int i=0; i<t; i++) {
-            lista[i] = t-i;		
+    public static DoubleLinkedList <Integer> peorCaso(int n){
+        DoubleLinkedList <Integer> lista = new DoubleLinkedList <Integer>();
+        for(int i = n; i > 0; i--){
+            lista.add(i);
         }
-        return lista;		
+        return lista;
     }
 
-    public static void main(String[] args) {
-
-        int tamano;		        
+    public static void main(String [] args) throws IOException{
+        int tamano;
         Scanner teclado = new Scanner( System.in );
         System.out.print( "Introduzca el tamaño máximo del último arreglo: " );
         tamano = teclado.nextInt();
-        
-        ArrayList<int[]> casos = new ArrayList<int[]>();        
-        for(int n=1; n<=tamano; n++) {
-            casos.add(generarPeorCaso(n));
-        }        
-        DoubleLinkedList<int[]> puntero = casos.DoubleLinkedList();
-        while(puntero.hasNext()){
-            System.out.println(puntero.next());
-        }
 
+        ArrayList<DoubleLinkedList<Integer>> casos = new ArrayList<>();
+        String archivoInsercion = "insercion.txt";
+        PrintWriter oS = new PrintWriter(archivoInsercion);
+
+        for(int n=1; n<=tamano; n++)
+            casos.add(peorCaso(n));
+        Iterator<DoubleLinkedList<Integer>> puntero = casos.iterator();
+        while(puntero.hasNext())
+            oS.println(String.valueOf(puntero.next().insertionSort()));
+        oS.close();
+
+        JavaPlot p = new JavaPlot();
+        p.addPlot("\"insercion.txt\" with lines");
+        p.plot();
     }
 
 }
+
